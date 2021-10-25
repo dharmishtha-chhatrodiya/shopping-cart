5cd120edb87fc13a1ff4185f09330068491a6538
dharmishtha-chhatrodiay <dharmichhatrodiya@gmail.com>
2021-10-25 16:25:49 +0530

    key add in to product list component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,15 +3,29 @@ import { productListData } from "./ProductListData.js";
 import Product from "./Product";
 import { Grid } from "semantic-ui-react";
 
+const styles = {
+  productList: {
+    margin: "0px 36px",
+    display: "flex",
+    textAlign: "center",
+    justifyContent: "center",
+  },
+};
+
 function ProductList(props) {
   return (
-    <Grid container>
+    <Grid style={styles.productList}>
       {/* Rendering Product List */}
+
       {productListData &&
-        productListData.map((u, i) => {
+        productListData.map((product) => {
           return (
-            <Grid.Column mobile={16} tablet={8} computer={5}>
-              <Product data={u} key={u.id} setopen={() => props.setopen()} />
+            <Grid.Column key={product.id} mobile={16} tablet={8} computer={5}>
+              <Product
+                data={product}
+                key={product.id}
+                setopen={() => props.setopen()}
+              />
             </Grid.Column>
           );
         })}
